c19b9f6c284f5c3f001ffe4e79c270f9992aa130
NerdyChris <cmashforth@gmail.com>
2017-11-26 21:54:10 +0000

    Added Timer that can be paused and resumed with UI updates accordingly

--- IChef/app/src/main/java/com/hci/team/ichef/IChef.java
+++ IChef/app/src/main/java/com/hci/team/ichef/IChef.java
@@ -1,8 +1,12 @@
 package com.hci.team.ichef;
 
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 /**
  * Created by Chris on 25/11/2017.
@@ -10,8 +14,60 @@ import android.support.v7.app.AppCompatActivity;
 
 public class IChef extends AppCompatActivity {
 
+    private Button startButton;
+    private Button pauseButton;
+    private Button resumeButton;
+    private TextView timerText;
+    private Long taskTime;
+    private Long timeRemaining;
+    private CountDownTimer timer;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ichef);
+        startButton = (Button) findViewById(R.id.startButton);
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+        resumeButton = (Button) findViewById(R.id.resumeButton);
+        timerText = (TextView) findViewById(R.id.timer);
+        taskTime = 300000L;
+        timeRemaining = 0L;
     }
+
+
+    public void onClick(View view){
+        if(view == startButton || view == resumeButton){
+            view.setVisibility(View.INVISIBLE);
+            pauseButton.setVisibility(View.VISIBLE);
+            createTimer(taskTime);
+        }
+        else if(view == pauseButton){
+            view.setVisibility(View.INVISIBLE);
+            resumeButton.setVisibility(View.VISIBLE);
+            timer.cancel();
+            taskTime = timeRemaining;
+        }
+    }
+
+
+    public void createTimer(Long time){
+        timer = new CountDownTimer(time,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerText.setText("Seconds Remaining: " + millisUntilFinished/1000);
+                timeRemaining = millisUntilFinished;
+            }
+
+            @Override
+            public void onFinish() {
+                timerText.setText("Finished");
+            }
+        }.start();
+    }
+
+
+
+
+
+
 }
